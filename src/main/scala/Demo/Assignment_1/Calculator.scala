8b16ffa0419d5ce86e0cbb9dc1550ba3f6c2f2a0
M Madhav Reddy <mmrsjce@gmail.com>
2023-11-04 12:09:05 +0530

    assignment1 updated

--- src/main/scala/Demo/Assignment_1/Calculator.scala
+++ src/main/scala/Demo/Assignment_1/Calculator.scala
@@ -1,8 +1,9 @@
-package Demo.Assignment_1
+package Demo.Assignment_1 //path
 
 class Calculator {
       def addition(a:Int,b:Int) : Int = a+b
       def subtract(a:Int,b:Int) : Int = a-b
       def multiplication(a:Int,b:Int) : Int = a*b
       def division(a:Int,b:Int) : Int = a/b
+
 }
